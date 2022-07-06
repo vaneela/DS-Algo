78ae79b0eda7203cbde2253c7d0de4eee733202c
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-07 02:56:44 +0530

    7-07-2022

---
+++ LinkedList/Intro.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node *next;
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void print(Node *head){
+    Node *temp = head;
+
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+void increment(Node *head){
+    Node *temp = head;
+    while(temp != NULL){
+        temp -> data++;
+        temp = temp -> next;
+    }
+}
+
+int main(){
+
+   Node *node1 = new Node(10);
+    Node *node2 = new Node(20);
+    Node *node3 = new Node(20);
+    Node *node4 = new Node(20);
+    node1 -> next = node2;
+    node2 -> next = node3;
+    node3 -> next = node4;
+    print(node2);
+}
