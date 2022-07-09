6b00d528a348b22e368d33c1acee0ff755c4e84e
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-09 14:11:17 +0530

    9-07-2022

--- LinkedList/Middle_of_linked_list_LC.cpp
+++ LinkedList/Middle_of_linked_list_LC.cpp
@@ -1,21 +1,39 @@
-   int size(ListNode *head){
-        int c = 0;
-        while(head!=NULL){
-            c++;
-            head=head->next;
-            }
-        return c;
-    }
-    ListNode* middleNode(ListNode* head) {
-        int n = size(head);
-        int mid = (n%2)?(n/2)+1:n/2 +1;
-        int c =0;
-        --mid;
-        ListNode * temp = head;
+//    int size(ListNode *head){
+//         int c = 0;
+//         while(head!=NULL){
+//             c++;
+//             head=head->next;
+//             }
+//         return c;
+//     }
+//     ListNode* middleNode(ListNode* head) {
+//         int n = size(head);
+//         int mid = (n%2)?(n/2)+1:n/2 +1;
+//         int c =0;
+//         --mid;
+//         ListNode * temp = head;
         
-        while(c!=mid){
-            temp = temp->next;
-            ++c;
+//         while(c!=mid){
+//             temp = temp->next;
+//             ++c;
+//         }
+//         return temp;
+//     }
+
+
+// Fast slow approach
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast = head;
+        if(head==NULL)
+            return head;
+        while(fast !=NULL && fast->next !=NULL  ){
+            slow =slow -> next;
+            fast = fast->next ->next;
         }
-        return temp;
+        return slow;
     }
+};
