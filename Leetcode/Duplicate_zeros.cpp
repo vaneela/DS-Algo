7dcf37d16792e6f4dc3883d29f1cdbff7c3d82ff
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-10 23:24:56 +0530

    10-07-2022

---
+++ Leetcode/Duplicate_zeros.cpp
@@ -0,0 +1,12 @@
+void duplicateZeros(vector<int>& arr) {
+    int n = arr.size();
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                
+                for(int j=arr.size()-1;j>=i+1;j--){
+                    arr[j]=arr[j-1];
+                }
+                ++i;
+            }
+        }
+    }
