f468164535f7df4a2ff0333ee981f5015418a027
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-14 03:14:04 +0530

    14-07-22

---
+++ LC_programmers_skill_study_plan_I/Day-1/Count_odd_nums_interval.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+    int ans=0;
+        if(!(low%2) && !(high%2)){
+            ans=(high-low)/2;
+        }else{
+            ans= (high-low)/2 +1;
+        }
+
+        return ans;
+    }
+}
