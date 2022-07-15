36daad107a9367e577c87a50d728521314b00bf6
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-16 01:49:10 +0530

    kickstart-june

--- KickStart/Sample_Problem.cpp
+++ KickStart/Sample_Problem.cpp
@@ -1,25 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 #define vi vector<int>
-#define ii pair<int,int>
+#define ii pair<int, int>
 
 using namespace std;
 // void solve(){
-    
+
 // }
-int main(){
+int main()
+{
     int t;
     cin >> t;
-    for(int x=1;x<=t;++x){
-        int n,m;
-        ll sum=0;
+    for (int x = 1; x <= t; ++x)
+    {
+        int n, m;
+        ll sum = 0;
         cin >> n >> m;
-        while(n--){
+        while (n--)
+        {
             int temp;
             cin >> temp;
             sum += temp;
         }
-        cout << "Case #" << x << ": " << sum%m << endl; 
+        cout << "Case #" << x << ": " << sum % m << endl;
     }
     return 0;
 }
