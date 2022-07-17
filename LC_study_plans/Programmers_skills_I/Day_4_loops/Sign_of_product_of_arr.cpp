32fcac5f3ebf9b170caa3d40403c8bacb331b164
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-18 02:00:46 +0530

    18-07-2022

---
+++ LC_study_plans/Programmers_skills_I/Day_4_loops/Sign_of_product_of_arr.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int ans =1, neg =0;
+        for(auto i : nums){
+            if(i==0)
+                return 0;
+            if(i<0)
+                neg++;
+        }
+        if(neg%2)
+            return -1;
+        return ans;
+    }
+};
