1aa2306ff0f3a6200f16378c915f8096a3b6e7ce
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-24 23:48:21 +0530

    24-07-2022

---
+++ LC_study_plans/Programmers_skills_I/Day-12(class and object)/Design_parking_system.cpp
@@ -0,0 +1,25 @@
+class ParkingSystem {
+public:
+    unordered_map<int,int>mp;
+    ParkingSystem(int big, int medium, int small) {
+        mp.insert({1,big});
+        mp.insert({2,medium});
+        mp.insert({3,small});
+
+    }
+    
+    bool addCar(int carType) {
+        if(mp[carType]==0)
+            return 0;
+        else{
+            mp[carType]--;
+            return 1;
+        }
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
