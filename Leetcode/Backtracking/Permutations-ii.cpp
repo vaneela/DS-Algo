bc2255b8335663c4ce3c042786be6787024c16a3
vaneelakhatri <vaneelakhatri09@gmail.com>
2022-07-26 23:38:34 +0530

    26-07-2022

--- Leetcode/Backtracking/Permutations-ii.cpp
+++ Leetcode/Backtracking/Permutations-ii.cpp
@@ -1,20 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    void solve(vector<int>&nums, int ind,vector<vector<int>> &ans){
-        if(ind>=nums.size()){
-            if(find(ans.begin(),ans.end(),nums)==ans.end())
-            ans.push_back(nums);
+    void solve(vector<int> &nums, int ind, vector<vector<int>> &ans)
+    {
+        if (ind >= nums.size())
+        {
+            if (find(ans.begin(), ans.end(), nums) == ans.end())
+                ans.push_back(nums);
             return;
         }
-        for(int i=ind;i<nums.size();i++){
-            swap(nums[i],nums[ind]);
-            solve(nums,ind+1,ans);
-            swap(nums[i],nums[ind]);
+        for (int i = ind; i < nums.size(); i++)
+        {
+            swap(nums[i], nums[ind]);
+            solve(nums, ind + 1, ans);
+            swap(nums[i], nums[ind]);
         }
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>ans;
-        solve(nums,0,ans);
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        solve(nums, 0, ans);
         return ans;
     }
 };
